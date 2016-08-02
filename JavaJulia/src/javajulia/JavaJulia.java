a6ffe6e80066fab5887a21943afd3388b921e992
Mircea <Mircea@inspiron>
2016-08-02 14:53:07 +0300

    I added a new line.

--- JavaJulia/src/javajulia/JavaJulia.java
+++ JavaJulia/src/javajulia/JavaJulia.java
@@ -16,6 +16,7 @@ public class JavaJulia {
      */
     public static void main(String[] args) {
        System.out.println("Hello " + args [0] + " and " + args [1] + ".");
+       System.out.println("I am on GitHub");
 
     }
     
